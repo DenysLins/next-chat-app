32f11b176d985b311c6d4d364d9c21414450e098
Denys Lins <denyslins@gmail.com>
2022-08-26 18:35:39 -0300

    Fix reset.js

--- reset.js
+++ reset.js
@@ -1,6 +1,13 @@
-const jsonDirectory = path.join(process.cwd(), "store");
-const messagesData = { messages: [] };
-const stringData = JSON.stringify(messagesData, null, 2);
-await fs.writeFile(jsonDirectory + "/messages.json", stringData, err => {
-  console.error(err);
-});
+const fs = require("fs");
+const path = require("path");
+
+const cleanMessages = async () => {
+  const jsonDirectory = path.join(process.cwd(), "store");
+  const messagesData = { messages: [] };
+  const stringData = JSON.stringify(messagesData, null, 2);
+  await fs.writeFile(jsonDirectory + "/messages.json", stringData, err => {
+    console.error(err);
+  });
+};
+
+cleanMessages();
