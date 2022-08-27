016d586b982610d72fb7e6d68cf402bd2a9f6522
Denys Lins <denyslins@gmail.com>
2022-08-27 11:01:45 -0300

    Add route to clear messages

---
+++ pages/api/clear.js
@@ -0,0 +1,14 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+const ApiHandler = async (req, res) => {
+  const jsonDirectory = path.join(process.cwd(), "store");
+  const messagesData = { messages: [] };
+  const stringData = JSON.stringify(messagesData, null, 2);
+  await fs.writeFile(jsonDirectory + "/messages.json", stringData, err => {
+    console.error(err);
+  });
+  res.status(200).json(JSON.parse(stringData));
+};
+
+export default ApiHandler;
