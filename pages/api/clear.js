cf051c521dc28e59ed6d73a438d0d2cf07b8a5c5
Denys Lins <denyslins@gmail.com>
2022-08-27 11:10:59 -0300

    Add eol to messages

--- pages/api/clear.js
+++ pages/api/clear.js
@@ -5,9 +5,13 @@ const ApiHandler = async (req, res) => {
   const jsonDirectory = path.join(process.cwd(), "store");
   const messagesData = { messages: [] };
   const stringData = JSON.stringify(messagesData, null, 2);
-  await fs.writeFile(jsonDirectory + "/messages.json", stringData, err => {
-    console.error(err);
-  });
+  await fs.writeFile(
+    jsonDirectory + "/messages.json",
+    stringData + "\n",
+    err => {
+      console.error(err);
+    }
+  );
   res.status(200).json(JSON.parse(stringData));
 };
 
